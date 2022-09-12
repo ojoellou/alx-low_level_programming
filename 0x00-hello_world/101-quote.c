26cdaeef95658e07d2383db7cfe37df6d37ce46a
ojoellou <ojoellois@yahoo.com>
2022-09-12 02:34:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main: Entry point
+ * main - Entry point
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	char sentence[]=
+	char sentence[] =
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, sentence, 59);
