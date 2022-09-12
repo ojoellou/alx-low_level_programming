e097cf80ac12b96c0edf917b83e589b5bca8e48f
ojoellou <ojoellois@yahoo.com>
2022-09-12 02:15:37 -0700

    betty style edit 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 
+		2015-10-19\n";
+	
 	write(2, sentence, 59);
 	return (1);
 }
