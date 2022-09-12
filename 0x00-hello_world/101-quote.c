f70e2d558921ef621dea92164cd98d8b9caa48ea
ojoellou <ojoellois@yahoo.com>
2022-09-12 02:10:48 -0700

    edited 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, sentence, sizeof(sentence));
+	write(2, sentence, 59);
 	return (1);
 }
