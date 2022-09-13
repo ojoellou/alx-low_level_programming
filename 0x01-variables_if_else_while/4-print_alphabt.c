95a0e4c551a2c319a8ffd6938debe5a7b6f75e96
ojoellou <ojoellois@yahoo.com>
2022-09-12 21:54:10 -0700

    edit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,19 +2,19 @@
 
 /**
  * main - entry point
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
+	for (i = 'a'; i <= 'z'; i++)
+	{
 		if (i != 'e' || i != 'q')
 		{
                 	putchar(i);
 		}
-        }
-        putchar("\n");
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
