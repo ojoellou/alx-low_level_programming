168a1d0e2802ec142f5cb53feb156fcaf439c06c
ojoellou <ojoellois@yahoo.com>
2022-09-12 21:26:57 -0700

    adding all to repo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf("\n");
+	return (0);
+}
+
