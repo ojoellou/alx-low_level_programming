aec63a22ee0f7227289987405947b3718abbf24d
ojoellou <ojoellois@yahoo.com>
2022-09-13 10:56:30 -0700

    edited

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
